26308b79f665f4ddd01c7146be0a51ed15b0b49e
Raidsoul <Thunderock01@gmail.com>
2018-10-11 00:50:53 +0700

    imdone

--- testfont/font.py
+++ testfont/font.py
@@ -1,9 +1,11 @@
+#!/usr/bin/python
+
 import cv2
 import numpy as np
+import os
 from PIL import Image
 from pytesseract import image_to_string
 
-img = cv2.imread('1.jpg')
-text = image_to_string(img,lang='eng')
+img = Image.open('E:\\OPENCVGITHUB\\opencv\\testfont\\1.jpg')
+text = image_to_string(img)
 print(text)
-
