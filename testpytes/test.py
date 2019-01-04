fae10b6b189d204a8b57cf9cc135daac6293156f
RAIDSouL <Thunderock01@gmail.com>
2019-01-04 07:39:56 +0700

    tha lang is acceptable

--- testpytes/test.py
+++ testpytes/test.py
@@ -1,6 +1,6 @@
 import ocr
 print(ocr.text_from_image_file('test-eng.PNG','eng')) # ดึงไฟล์ test-eng.PNG เข้ามา โดยกำหนดเป็นภาษาอังกฤษ
-#print(ocr.text_from_image_file('test-thai.PNG','thai')) # ดึงไฟล์ test-thai.PNG เข้ามา โดยกำหนดเป็นภาษาไทย
+print(ocr.text_from_image_file('test-thai.PNG','tha')) # ดึงไฟล์ test-thai.PNG เข้ามา โดยกำหนดเป็นภาษาไทย
 print(ocr.text_from_image_file('4.PNG','eng')) # กำหนดเป็นภาษาอังกฤษ
 print(ocr.text_from_image_file('5.PNG','eng')) # กำหนดเป็นภาษาอังกฤษ
 print(ocr.text_from_image_file('6.PNG','eng')) # กำหนดเป็นภาษาอังกฤษ
